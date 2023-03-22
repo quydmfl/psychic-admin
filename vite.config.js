e5a0682ff9322a2828239670b6a4e921193c71a8
phantien295 <phantien295@gmail.com>
2023-03-23 01:19:34 +0700

    [Fix] vite build config

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslint from "vite-plugin-eslint";
 import eslintPlugin from "vite-plugin-eslint";
-import path from 'path'
+import path from "path";
 
 export default defineConfig({
   plugins: [vue(), eslint({ fix: true }), eslintPlugin()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
-  }
+  },
+  build: {
+    chunkSizeWarningLimit: 1024,
+  },
 });
